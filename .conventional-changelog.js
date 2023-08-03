f19574028e16b901104bc4b69a9790ef0e427c57
Sergej <64812274+SergejBusch@users.noreply.github.com>
2023-08-03 11:11:51 +0200

    feat: add 8

---
+++ .conventional-changelog.js
@@ -0,0 +1,8 @@
+module.exports = {
+    types: [
+        { type: 'feat', section: 'Features' },
+        { type: 'feature', section: 'Features' },
+        { type: 'fix', section: 'Bug Fixes' },
+        { type:  'refactor', section: 'Refactor'}
+    ],
+};
